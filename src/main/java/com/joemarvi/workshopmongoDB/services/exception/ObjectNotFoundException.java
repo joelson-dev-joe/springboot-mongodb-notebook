236a4c3e09f953640536cf3cdf7a8fc1d55722c9
joelson-dev-joe <joelson.dh@gmail.com>
2020-12-29 08:18:19 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/joemarvi/workshopmongoDB/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.joemarvi.workshopmongoDB.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+}
